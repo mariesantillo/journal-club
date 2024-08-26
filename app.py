c8236cb24f5acdf441e2881685d84e4ae0ffd40b
mariesantillo <santillo.marie@gmail.com>
2024-08-26 16:51:17 +0100

    Change to firebase

--- app.py
+++ app.py
@@ -1,34 +1,26 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from firebase_admin import credentials, firestore, initialize_app
-from flask_migrate import Migrate  
 from wtforms import StringField, PasswordField, BooleanField, FileField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
-import os
 import random 
 
-# Ensure this line is added to set the environment variable within your application
-os.environ['FIREBASE_CREDENTIALS_PATH'] = '/Users/mariefrancine/Desktop/journal_club/secrets/journalclub-6a9bb-firebase-adminsdk-vqslj-2958062728.json'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
+# Initialize Firebase Admin SDK
 firebase_cred_path = os.getenv('FIREBASE_CREDENTIALS_PATH')
 
 if firebase_cred_path:
     cred = credentials.Certificate(firebase_cred_path)
     initialize_app(cred)
+    db = firestore.client()  # Initialize Firestore client
 else:
     print("Firebase credentials path not set. Please set the FIREBASE_CREDENTIALS_PATH environment variable.")
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thisisasecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -36,20 +28,26 @@ login_manager.login_view = 'login'
 
 EMOJI_LIST = ['🦕', '🐹', '🐰', '🦊', '🐼', '🐷', '🐨', '🐝', '🐞', '🐥', '🐙', '🦭', '🦦', '🦔', '🐧', '🐯', '🫎', '🐢', '🐳', '🐮']  # Define a list of emojis
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-    emoji = db.Column(db.String(20))  # New column to store user's emoji
+# class User(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(150), unique=True, nullable=False)
+#     password = db.Column(db.String(150), nullable=False)
+#     emoji = db.Column(db.String(20))  # New column to store user's emoji
 
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150), nullable=False)
-    file_path = db.Column(db.String(150), nullable=False)
-    votes = db.Column(db.Integer, default=0)
-    emoji_votes = db.Column(db.String, default='')  # New column to store emojis of voters
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class User(UserMixin):
+    def __init__(self, user_id, username, password, emoji):
+        self.id = user_id
+        self.username = username
+        self.password = password
+        self.emoji = emoji
 
+# class Article(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(150), nullable=False)
+#     file_path = db.Column(db.String(150), nullable=False)
+#     votes = db.Column(db.Integer, default=0)
+#     emoji_votes = db.Column(db.String, default='')  # New column to store emojis of voters
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class SettingsForm(FlaskForm):
     # email_reminders = BooleanField('Receive email reminders for upcoming meetings')
@@ -62,7 +60,11 @@ with app.app_context():
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    user_ref = db.collection('users').document(user_id).get()
+    if user_ref.exists:
+        user_data = user_ref.to_dict()
+        return User(user_id=user_id, username=user_data['username'], password=user_data['password'], emoji=user_data['emoji'])
+    return None
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -92,13 +94,14 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            return redirect(url_for('dashboard'))
-
+        user_ref = db.collection('users').document(form.username.data).get()
+        if user_ref.exists:
+            user_data = user_ref.to_dict()
+            user = User(user_id=form.username.data, username=user_data['username'], password=user_data['password'], emoji=user_data['emoji'])
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
         flash('Invalid username or password')
-
     return render_template('login.html', form=form)
 
 @app.route('/user')
@@ -113,13 +116,21 @@ def register():
 
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data)
-        # Assign a random emoji from the list
         assigned_emoji = random.choice(EMOJI_LIST)
-        new_user = User(username=form.username.data, password=hashed_password, emoji=assigned_emoji)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Registration successful! You can now log in.')
-        return redirect(url_for('login'))
+        user_data = {
+            'username': form.username.data,
+            'password': hashed_password,
+            'emoji': assigned_emoji
+        }
+
+        # Save user data to Firestore
+        user_ref = db.collection('users').document(form.username.data)
+        if user_ref.get().exists:
+            flash('Username already taken. Please choose a different one.')
+        else:
+            user_ref.set(user_data)
+            flash('Registration successful! You can now log in.')
+            return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
 
@@ -129,38 +140,49 @@ def dashboard():
     form = UploadForm()
     if form.validate_on_submit():
         file = form.file.data
-        upload_folder = app.config['UPLOAD_FOLDER']
-
-        # Ensure the upload directory exists
-        if not os.path.exists(upload_folder):
-            os.makedirs(upload_folder)
-
-        file_path = os.path.join(upload_folder, file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(file_path)
 
-        new_article = Article(title=form.title.data, file_path=file_path, user_id=current_user.id)
-        db.session.add(new_article)
-        db.session.commit()
+        article_data = {
+            'title': form.title.data,
+            'file_path': file_path,
+            'votes': 0,
+            'emoji_votes': '',
+            'user_id': current_user.id
+        }
+
+        # Save article data to Firestore
+        article_ref = db.collection('articles').document()
+        article_ref.set(article_data)
+
         flash('Article uploaded successfully!')
 
-    articles = Article.query.all()
-    return render_template('dashboard.html', form=form, articles=articles)
+    # Fetch all articles from Firestore
+    articles = db.collection('articles').stream()
+    articles_list = [{'id': article.id, **article.to_dict()} for article in articles]
 
-@app.route('/vote/<int:article_id>')
+    return render_template('dashboard.html', form=form, articles=articles_list)
+
+@app.route('/vote/<article_id>')
 @login_required
 def vote(article_id):
-    article = Article.query.get_or_404(article_id)
-    
-    # Check if the current user's emoji is already in the vote list to prevent duplicate voting
-    if current_user.emoji not in article.emoji_votes:
-        article.votes += 1
-        article.emoji_votes += current_user.emoji  # Add user's emoji to the vote list
-        db.session.commit()
-        flash('Vote cast successfully!')
+    article_ref = db.collection('articles').document(article_id)
+    article = article_ref.get()
+
+    if article.exists:
+        article_data = article.to_dict()
+        if current_user.emoji not in article_data['emoji_votes']:
+            article_data['votes'] += 1
+            article_data['emoji_votes'] += current_user.emoji
+            article_ref.update(article_data)
+            flash('Vote cast successfully!')
+        else:
+            flash('You have already voted for this article!')
     else:
-        flash('You have already voted for this article!')
+        flash('Article not found.')
 
     return redirect(url_for('index'))
+
 
 
 @app.route('/logout')
