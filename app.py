e9a7eb531eb247a96284621a59e6d7e637f55ef0
mariesantillo <santillo.marie@gmail.com>
2024-08-27 09:59:00 +0100

    fix mistake

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-Copy code
 from flask import Flask, render_template, redirect, url_for, request, flash, send_file
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
